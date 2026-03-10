d218896db00c9e1eaae9a0aa8461d83a91f9bac8
Prince Chrix <134553523+princechrix@users.noreply.github.com>
2026-03-10 09:19:15 +0200

    [ADD]: added vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
